751a9687d464fddb3608eafb225aa7a98477803d
Osama Al-Hussein <osamahosen99@gmail.com>
2026-08-25 12:23:26 +0300

    Add example of virtual functions in C++ OOP
    Implement virtual functions in OOP with derived classes.

---
+++ The concept of object-oriented programming(OOP)/Virtual Function.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std ;
+
+class clsPerson{
+    public:
+    
+    virtual void Print(){
+        cout << "\nHi I am a Person\n";
+    }
+};
+
+class clsEmployee : public clsPerson {
+
+    public:
+    void Print(){
+        cout << "\nHi I am an Employee\n";
+    }
+
+};
+
+
+class clsStudent : public clsPerson{
+    public:
+    void Print(){
+        cout << "\nHi I am a Student\n";
+    }
+};
+
+int main(){
+
+    clsEmployee Employee1 ;
+    clsStudent Student1 ;
+
+    Employee1.Print();
+    Student1.Print();
+
+
+    clsPerson * Person1 = &Employee1 ;
+    clsPerson * Person2 = &Student1 ;
+
+    Person1->Print();
+    Person2->Print();
+
+    return 0;
+}
